4522412f9081c837c5f0547ad3e0782cb6ea39e6
Antonio Román <kyradiscord@gmail.com>
2020-08-21 00:26:26 +0200

    feat: export AliasPiece and AliasStore in index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from './lib/Piece';
 export * from './lib/Store';
+export * from './lib/AliasPiece';
+export * from './lib/AliasStore';
 export * from './lib/errors/LoaderError';
 export * from './lib/strategies/ILoader';
 export * from './lib/strategies/LoadSingle';
