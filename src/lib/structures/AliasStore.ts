d02989b109c1e178e1f592ce12fe3fbc9b0dc6fb
Soumil <soumil.paranjpay@gmail.com>
2020-12-29 21:34:04 +0530

    fix: lint errors

--- src/lib/structures/AliasStore.ts
+++ src/lib/structures/AliasStore.ts
@@ -6,55 +6,55 @@ import { Store } from './Store';
  * The store class which contains [[AliasPiece]]s.
  */
 export class AliasStore<T extends AliasPiece> extends Store<T> {
-    /**
-     * The aliases referencing to pieces.
-     */
-    public readonly aliases = new Collection<string, T>();
+	/**
+	 * The aliases referencing to pieces.
+	 */
+	public readonly aliases = new Collection<string, T>();
 
-    /**
-     * Looks up the name by the store, falling back to an alias lookup.
-     * @param key The key to look for.
-     */
-    public get(key: string): T | undefined {
-        return super.get(key) ?? this.aliases.get(key);
-    }
+	/**
+	 * Looks up the name by the store, falling back to an alias lookup.
+	 * @param key The key to look for.
+	 */
+	public get(key: string): T | undefined {
+		return super.get(key) ?? this.aliases.get(key);
+	}
 
-    /**
-     * Checks whether a key is in the store, or is an alias
-     * @param key The key to check
-     */
-    public has(key: string): boolean {
-        return super.has(key) || this.aliases.has(key);
-    }
+	/**
+	 * Checks whether a key is in the store, or is an alias
+	 * @param key The key to check
+	 */
+	public has(key: string): boolean {
+		return super.has(key) || this.aliases.has(key);
+	}
 
-    /**
-     * Unloads a piece given its instance or its name, and removes all the aliases.
-     * @param name The name of the file to load.
-     * @return Returns the piece that was unloaded.
-     */
-    public unload(name: string | T): Promise<T> {
-        const piece = this.resolve(name);
+	/**
+	 * Unloads a piece given its instance or its name, and removes all the aliases.
+	 * @param name The name of the file to load.
+	 * @return Returns the piece that was unloaded.
+	 */
+	public unload(name: string | T): Promise<T> {
+		const piece = this.resolve(name);
 
-        // Unload all aliases for the given piece:
-        for (const alias of piece.aliases) {
-            // We don't want to delete aliases that were overriden by another piece:
-            const aliasPiece = this.aliases.get(alias);
-            if (aliasPiece === piece) this.aliases.delete(alias);
-        }
+		// Unload all aliases for the given piece:
+		for (const alias of piece.aliases) {
+			// We don't want to delete aliases that were overriden by another piece:
+			const aliasPiece = this.aliases.get(alias);
+			if (aliasPiece === piece) this.aliases.delete(alias);
+		}
 
-        return super.unload(piece);
-    }
+		return super.unload(piece);
+	}
 
-    /**
-     * Inserts a piece into the store, and adds all the aliases.
-     * @param piece The piece to be inserted into the store.
-     * @return The inserted piece.
-     */
-    protected async insert(piece: T) {
-        for (const key of piece.aliases) {
-            this.aliases.set(key, piece);
-        }
+	/**
+	 * Inserts a piece into the store, and adds all the aliases.
+	 * @param piece The piece to be inserted into the store.
+	 * @return The inserted piece.
+	 */
+	protected async insert(piece: T) {
+		for (const key of piece.aliases) {
+			this.aliases.set(key, piece);
+		}
 
-        return super.insert(piece);
-    }
+		return super.insert(piece);
+	}
 }
